b4bef8d3550663463b960fd033626952da480f10
Analecia <marianaanalecia@gmail.com>
2023-12-01 21:23:43 -0300

    missao5

---
+++ Procedimento2/CadCliente/Saida.java
@@ -0,0 +1,26 @@
+package cadastroclient;
+import javax.swing.*;
+/**
+ *
+ * @author grego
+ */
+public class SaidaFrame extends JDialog {
+    public JTextArea texto;
+
+    public SaidaFrame() {
+        // Define as dimensões da janela
+        setBounds(100, 100, 400, 300);
+
+        // Define o status modal como false
+        setModal(false);
+
+        // Acrescenta o componente JTextArea na janela
+        texto = new JTextArea(25, 40);
+        texto.setEditable(false); // Bloqueia edição do campo de texto
+        
+        // Adiciona componente para rolagem
+        JScrollPane scroll = new JScrollPane(texto);
+        scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER); // Bloqueia rolagem horizontal
+        add(scroll);
+    }
+}
